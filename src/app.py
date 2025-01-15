be6318909e61a598b29727f96d11e08845b78797
enkelii6 <kukarsevgleb@gmail.com>
2025-01-15 07:33:42 +0500

    hotfix: cors

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import uvicorn
 from litestar import Litestar
+from litestar.config.cors import CORSConfig
 from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyInitPlugin
 
 from src.config import settings
@@ -7,10 +8,13 @@ from src.routes import routes
 
 db_config = SQLAlchemyAsyncConfig(connection_string=str(settings.database_url))
 
+cors_config = CORSConfig(allow_origins=['*'], allow_headers=['*'], allow_methods=['*'])
+
 app = Litestar(
     route_handlers=routes,
     plugins=[SQLAlchemyInitPlugin(config=db_config)],
     debug=True,
+    cors_config=cors_config,
 )
 
 
